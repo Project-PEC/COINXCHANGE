7a274c6b1325a85e73f553c68487dd003fd9258e
Shubham Arya <sandm3766@gmail.com>
2021-11-03 00:31:57 +0530

    Navbar edits

--- f_app/src/components/Navbar/Navbar.js
+++ f_app/src/components/Navbar/Navbar.js
@@ -112,7 +112,7 @@ const Navbar = ({ username, setUsername, socket, unread, setUnread }) => {
             </Dropdown.Toggle>
 
             <Dropdown.Menu variant="dark" className="dropdownMenu">
-              <Dropdown.Item as={Link} to={"/profile/" + username}>Profile</Dropdown.Item>
+              <Dropdown.Item className="weex" as={Link} to={"/profile/" + username}>Profile</Dropdown.Item>
               <Dropdown.Item><Button onClick={username ? logOut : faltu} link={username ? '/' : '/sign-up'} buttonStyle='btn--outline'>{username ? "LogOut" : "SIGN UP"}</Button></Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
