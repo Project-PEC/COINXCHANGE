3fd0ad52038111496d20ad5a813d1af45f3a0270
Arsh Kashyap <arshk102001@gmail.com>
2021-11-15 15:50:54 +0530

    delete glitch fixed

--- f_app/src/components/ShowCoin/ShowCoin.js
+++ f_app/src/components/ShowCoin/ShowCoin.js
@@ -14,7 +14,8 @@ import { RatingView } from 'react-simple-star-rating';
 import { MdLocationOn } from "react-icons/md";
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Button , ButtonN } from "../Button/Button";
+import {  ButtonN } from "../Button/Button";
+import Button from 'react-bootstrap/Button';
 
 
 toast.configure();
@@ -25,6 +26,7 @@ const ShowCoin = (props) => {
     const [rating, setRating] = useState(0);
     // console.log(props);
 
+    console.log("Chala");
     useEffect(async () => {
         if (!props.location.param1) {
             props.history.push('/');
@@ -59,8 +61,8 @@ const ShowCoin = (props) => {
 
     const deleteHandler = async () => {
         const x = await deleteCoin(coin._id);
-        toast("Coin Deleted Successfully!");
         props.history.push('/services'); //not working!!
+        toast("Coin Deleted Successfully!");
         // props.history.push('/services'); //not working!!
     }
 
