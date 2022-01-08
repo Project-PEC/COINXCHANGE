cc4536c41e12104158053337f12b321385d2ba40
Arsh Kashyap <arshk102001@gmail.com>
2022-01-08 17:51:16 +0530

    cors issue

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(session({
 
 }))
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://coinxchange.herokuapp.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
