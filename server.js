a740143356290c39ce2e59d5ec9c58bae2ca7644
Arsh Kashyap <arshk102001@gmail.com>
2022-01-08 20:13:44 +0530

    production mode

--- server.js
+++ server.js
@@ -60,11 +60,7 @@ app.use("/backend/", reviewRoutes);
 
 if(process.env.NODE_ENV==='production')
 {
-  const publicPath = path.join(__dirname, 'f_app', 'build');
-	app.use(express.static(publicPath));
-	app.get('*', (req, res) => {
-	res.sendFile(path.join(publicPath,'index.html'));
-	});
+  app.use(express.static("f_app/build"))
 }
 
 app.listen(PORT, console.log(`Server is starting at ${PORT}`));
